9176521da87bd6250be4fcbc6a889802eb7c93e6
Anisha Sharma <anisha.sharma@solirius.com>
2025-10-07 16:34:41 +0100

    origin

---
+++ app/controllers/resources_controller.rb
@@ -0,0 +1,30 @@
+class ResourcesController < ApplicationController
+   def index
+        @resources = Resource.all
+   end
+
+   def new
+        @resource = Resource.new()
+   end
+
+   def create
+        @resource = Resource.new(resource_params)
+        if @resource.save
+            redirect_to root_url
+        else
+            render :new
+        end
+   end
+
+   def destroy
+       Resource.find(params[:id]).destroy
+       redirect_to root_url
+   end
+
+   private
+
+   def resource_params
+       params.require(:resource).permit(:name, :link)
+   end
+
+end
